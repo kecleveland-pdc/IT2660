30832fd3dbb5b9748f039cfce5d7313477714907
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-01-27 17:00:42 -0500

    Adding all initial files. Created Listing class.

---
+++ A01/JavaReview/src/javareview/Listing.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javareview;
+
+/**
+ *
+ * @author Keigh
+ */
+public class Listing {
+    private String _name;
+    private int _age;
+    
+    public Listing()
+    {
+        _name = " ";
+        _age = 0;
+    }
+    
+    public Listing(String name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+    
+    public String getName()
+    {
+        return _name;
+    }
+    
+    public void setName(String name)
+    {
+        _name = name;
+    }
+    
+    public int getAge()
+    {
+        return _age;
+    }
+    
+    public void setAge(int age)
+    {
+        _age = age;
+    }
+    
+    public void input(String name, int age)
+    {
+       this._name = name;
+       this._age = age;
+    }
+    
+    public String toString()
+    {
+        return this._name;
+    }
+}
