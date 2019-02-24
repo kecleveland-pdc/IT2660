79b65950aea5961c882cda5e0d365e2aa17af3fa
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-02-23 19:08:29 -0500

    Renamed StudentListing class to Node class...again. Added getters/settters for Node class.  Added Students class. Including Experiments class.

---
+++ A02/StudentListingsApp/src/studentlistingsapp/Students.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlistingsapp;
+
+/**
+ *
+ * @author Keigh
+ */
+public class Students {
+   Node[] studentArray;
+  
+    public void insert(Node[] studentArray)
+    {
+    
+    }
+   
+    public void delete(Node[] studentArray)
+    {
+    
+    }
+    
+    public void update(Node[] studentArray)
+    {
+    
+    }
+    
+    public int fetch(Node[] studentArray)
+    {
+        return 0; //placeholder
+    }
+
+}
