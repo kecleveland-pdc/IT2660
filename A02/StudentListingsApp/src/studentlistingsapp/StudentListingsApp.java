a501aa2a4a6e4e6aeb8d236d903cea2df7392396
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-03-09 09:37:24 -0500

    Added GetStudentRecords and  Run methods in StudenListingsApp.

--- A02/StudentListingsApp/src/studentlistingsapp/StudentListingsApp.java
+++ A02/StudentListingsApp/src/studentlistingsapp/StudentListingsApp.java
@@ -16,38 +16,50 @@ public class StudentListingsApp {
         Scanner scanUtil = new Scanner(System.in);
         int maxDataSet = 0;
         int initNumOfStudents = 0;
+        String studentDataSet = "";
         
-        System.out.print("Student Records App");
         try
         {
-            RunSetUp(scanUtil);
-            System.out.print("Student Records App");
-            System.out.print("-------------------");
-            System.out.print("Please enter the maximum size of the data set:");
-            maxDataSet = Integer.parseInt(scanUtil.nextLine());
-            System.out.print("Please enter initial number of students:");
-            initNumOfStudents = Integer.parseInt(scanUtil.nextLine());   
-            System.out.print("Please enter initial student data set in the folllwing format:\n");
-            System.out.print("StudentName1,StudentAddress1,StudentNumber1;StudentName2,StudentAddress2,StudentNumber2;...");
+            Run(scanUtil, maxDataSet, initNumOfStudents, studentDataSet);       
+            
          }catch(Exception ex){
             System.out.println("Error processing: " + ex.toString());
          }
     }     
     
-    public static void RunSetUp(Scanner scanUtil)
+    public static void Run(Scanner scanUtil, int maxDataSet, int initNumberOfStudents, String studentDataSet)
     {
+
+        String initDataSet;
+        System.out.println("Student Records App ");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Please enter the maximum size of the data set: ");
+        maxDataSet = Integer.parseInt(scanUtil.nextLine());
+        
+        System.out.println("Please enter initial number of students: ");
+        initNumberOfStudents = Integer.parseInt(scanUtil.nextLine());   
+        
+        System.out.println("Please enter initial student data set in the folllwing format: ");
+        System.out.println("StudentName1,StudentAddress1,StudentNumber1;StudentName2,StudentAddress2,StudentNumber2;...");
+        initDataSet = scanUtil.nextLine();
+        //System.out.println(initDataSet);
     
     }
     
-    public static void CreateInitialDataSet(List<String> initDataSet)
+    public static StudentRecords GetStudentRecords(StudentRecords studentRecords, String studentDataSet)
     {
-        for(String student: initDataSet)
+        String[] students = studentDataSet.split(";");
+        for(String student: students)
         {
-            String name = "";
-            String address  = "";
-            String number = "";
+            String[] studentData = student.split(",");
+            String name = studentData[0];
+            String address = studentData[1];
+            String number = studentData[2];
             
             Node studentNode = new Node(name, address, number);
+            studentRecords.insert(studentNode);
         }
+        
+        return studentRecords;
     }
 }
