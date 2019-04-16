0db921f5ed4cc3f8c88c6cbe50d11e4a9a6d8355
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-04-16 18:01:18 -0400

    Updated main to run checks for overflow/underflow and to peek. Added methods to manipulate stack.

--- A03/RestrictedDataStructures/src/restricteddatastructures/RestrictedDataStructures.java
+++ A03/RestrictedDataStructures/src/restricteddatastructures/RestrictedDataStructures.java
@@ -15,7 +15,35 @@ public class RestrictedDataStructures {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Stack stack = new Stack(3);
+        Listing li = new Listing("Keaunna", "123 Street", "123 1111");
+        Listing li2 = new Listing("Miya", "111 Street", "123 0000");
+        Listing li3 = new Listing("Sharlene", "456 Street", "123 4444");
+        Listing li4 = new Listing("Expander", "000 Street", "000 0000");
+        Listing popped;
+        
+        //checking underflow & overflow
+        System.out.println(stack.checkUnderflow());
+        System.out.println(stack.checkOverflow());
+        
+        //pushing on stack
+        stack.push(li);
+        stack.push(li2);
+        stack.push(li3);
+        
+        //expanding full stack
+        stack.showAll();
+        stack.push(li4);  //should expand stack
+        stack.showAll();
+        
+        //popping off stack
+        popped = stack.pop();
+        System.out.println("This was popped:" + popped); //should be "Expander"
+        stack.showAll();
+        
+        //peeking
+        System.out.println("Peeking..." + stack.peek());
+        
     }
     
 }
--- A03/RestrictedDataStructures/src/restricteddatastructures/Stack.java
+++ A03/RestrictedDataStructures/src/restricteddatastructures/Stack.java
@@ -10,28 +10,99 @@ package restricteddatastructures;
  * @author Keigh
  */
 public class Stack {
-    private Listing[] data;
-    private int top;
-    private int size;
+    private Listing[] _data;
+    private int _top;
+    private int _size;
     
     public Stack (int n)
     {
-        top = -1;
-        size = n;
-        data = new Listing[n];
+        _top = -1;
+        _size = n;
+        _data = new Listing[n];
     }
     
     public boolean push(Listing newNode)
     {
-        if(top == size - 1)
+        if(_top == _size - 1)
         {
-            return false; 
+            Listing[] larger = new Listing[_size+1];
+            
+            ///expand stack
+            Listing temp[] = _data;
+            _data = larger;
+            for(int i = 0; i <= temp.length-1; i++)
+            {
+                _data[i] = temp[i];
+            }
+            
+            temp = null;
+            larger = null;  
+        }
+
+        _top = _top + 1;
+        _data[_top] = newNode.deepCopy();
+        return true;
+    }
+    
+    public Listing pop()
+    {
+        int topLocation;
+        if(_top == -1)
+        {
+            return null;
         }
         else
         {
-            top = top + 1;
-            data[top] = newNode.deepCopy();
-            return true;
+            topLocation = _top;
+            _top = _top - 1;
+            return _data[topLocation];
+        }
+    }
+    
+    public void reInitToEmpty()
+    {
+        _top = -1;
+    }
+    
+    public String checkUnderflow()
+    {
+        if(_top == -1)
+        {
+            return "The stack is empty. A pop operation on this stack will result in an underflow condition.";
+        }
+        
+        return "The stack is not empty. A pop operation on this stack will not result in an underflow condition.";
+    }
+    
+    public String checkOverflow()
+    {
+        if(_top == _size-1)
+        {
+            return "The stack is full. A push operation on this stack will result in an overflow condition.";
+        }
+        
+        return "The stack is not full. A push operation on this stack will not result in an overflow condition";
+    }
+    
+    public Listing peek()
+    {
+        int topLocation;
+        if(_top == -1)
+        {
+            return null;
+        }
+        else
+        {
+            topLocation = _top;
+            return _data[topLocation];
+        }
+    }
+    
+    public void showAll()
+    {
+        for(int i = _top; i >= 0; i--)
+        {
+            System.out.println(_data[i].toString());
         }
     }
 }
