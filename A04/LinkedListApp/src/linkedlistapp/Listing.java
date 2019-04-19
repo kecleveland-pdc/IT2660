6e6e415606e572d7f77719758e75da5113bf1b96
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-04-19 11:29:21 -0400

    Added LinkedListDriver and Listing classes as well as LinkedListApp for linked lists.

---
+++ A04/LinkedListApp/src/linkedlistapp/Listing.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlistapp;
+
+/**
+ *
+ * @author Keigh
+ */
+public class Listing {
+    private String _name;
+    private String _number;
+    private String _address;
+    
+    public Listing()
+    {
+        _name = "";
+        _address = "";
+        _number = "";
+    }
+    
+    public Listing(String name, String number, String address)
+    {
+        _name = name;
+        _number = number;
+        _address = address;
+    }
+    
+    public String getName()
+    {
+        return _name;
+    }
+    
+    public void setName(String name)
+    {
+        _name = name;
+    }
+    
+    public String getNumber()
+    {
+        return _number;
+    }
+    
+    public void setNumber(String number)
+    {
+        _number = number;
+    }
+    
+    @Override
+    public String toString()
+    {
+        if (_name.equals(" ")) {
+            _name = "N/A";
+        }
+        
+        return "Name: " + _name +"\n";
+    }
+    
+    public Listing deepCopy()
+    {
+        Listing clone = new Listing(_name, _address, _number);
+        return clone;
+    }
+}
