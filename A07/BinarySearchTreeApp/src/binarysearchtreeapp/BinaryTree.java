cbe91fdfa8a67497fdaad4a30499050b64ceb9b5
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-05-06 21:45:59 -0400

    Deleted Node and NodeWrapper classes and added them to BinaryTree class.

--- A07/BinarySearchTreeApp/src/binarysearchtreeapp/BinaryTree.java
+++ A07/BinarySearchTreeApp/src/binarysearchtreeapp/BinaryTree.java
@@ -17,4 +17,59 @@ public class BinaryTree {
         root = null; 
     }
     
+    public boolean insert(Listing newListing)
+    {
+        //create parent/child node wrappers
+        NodeWrapper nodeWrapperParent = new NodeWrapper();
+        NodeWrapper nodeWrapperChild = new NodeWrapper();
+        Node curNode = new Node();
+        
+        if(curNode == null)
+        {
+            return false; // no memory
+        }
+        else
+        {
+            curNode.listingNode = newListing.deepCopy();
+            curNode.leftChild = null;
+            curNode.rightChild  = null;
+        }
+        
+        return true;
+    }
+    
+    //needs to be part of BinaryTree
+    public class Node 
+    {
+
+        private Listing listingNode;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node() 
+        {
+        }
+    }
+    
+    //needs to be part of BinaryTree
+    public class NodeWrapper 
+    {
+        Node nodeRef = null;
+    
+        public NodeWrapper()
+        {
+        }
+    
+        public Node get()
+        {
+            return nodeRef;
+        }
+    
+        public void set(Node n)
+        {
+            nodeRef = n;
+        }
+    }
 }
+
+
